ae933057fe090d7fb9fed5cc49f8b75a06d69458
Tincho <martincalozo@gmail.com>
2022-09-19 08:39:16 -0300

    Primer Commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+    env: {
+        backendURL: 'http://localhost:4000',
+        frontendURL: 'http://localhost:3000'
+    }
 }
-
-module.exports = nextConfig
